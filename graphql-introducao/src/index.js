5243f9608e59971d2d4831b47280e7740efe9537
Larissa Fossato <92603640+Lafossato@users.noreply.github.com>
2022-10-18 11:21:33 -0300

    Initial commit

---
+++ graphql-introducao/src/index.js
@@ -0,0 +1,38 @@
+import { createServer } from '@graphql-yoga/node'
+
+const typeDefs = `
+type Livro {
+id: ID!
+titulo: String!
+genero: String!
+edicao: Int,
+preco: Float
+},
+type Query {
+    effectiveJava: Livro!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        effectiveJava() {
+            return {
+                id: '123456',
+                titulo: null,
+                titulo: 'Effective Java',
+                genero: 'Técnico',
+                edicao: 3,
+                preco: 43.9
+            }
+        }
+    },
+};
+const server = createServer({
+    schema: {
+        typeDefs,
+        resolvers
+    }
+})
+server.start(() => {
+    'Servidor no ar...'
+})
